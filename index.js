494d2d219d3c60b596205b0d43ebf73ab0445468
WillisLi <li.willis95@gmail.com>
2021-09-25 16:09:39 -0700

    set up router for endpoints

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+require("dotenv").config();
+
 const server = require('./genshinapi/server')
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
