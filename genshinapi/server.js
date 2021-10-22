6ad083bd65e77ad629c9ce17512cd2d5a22fe263
WillisLi <li.willis95@gmail.com>
2021-10-21 18:29:21 -0700

    minor fixes and added some templates

--- genshinapi/server.js
+++ genshinapi/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dataRouter = require('./router/api-router');
-const cors = require('cors')
+const cors = require('cors');
+const helmet = require('helmet');
 const server = express();
 
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
 server.use('/', dataRouter); 
 
 module.exports = server; 
